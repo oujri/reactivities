75ef8508db21e32d52c51cd1d54a52da43ac1b69
Anass Oujri <anassoujri@gmail.com>
2020-10-07 23:18:11 +0100

    Services error

---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly HttpContextAccessor _httpContextAccessor;
+
+        public UserAccessor(HttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetCurrentUsername()
+        {
+            var username = _httpContextAccessor.HttpContext.User?.Claims
+                ?.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            return username;
+        }
+    }
+}
